33903158c181debd485fe6ff9bc66cfde4a315b4
Admiral-Simo <admiralsimo@gmail.com>
2023-02-04 07:17:20 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Main from "./components/Main/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Main />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import LinkItem from "../LinkItem/LinkItem";
+
+const Main = () => {
+  return (
+    <section id="main" className="bg-gray-100">
+      <div className="container p-3 mx-auto mt-16 mb-9">
+        <div className="flex max-w-4xl mx-auto flex-col p-10 space-y-3 md:space-y-0 md:space-x-3 bg-darkViolet md:flex-row rounded-lg">
+          <input
+            className="rounded-lg p-4 placeholder-yellow-500 focus:outline-none md:flex-1"
+            type="text"
+            placeholder="Shorten a link here"
+          />
+          <button className="btn rounded-lg text-white p-4">Shorten it!</button>
+        </div>
+      </div>
+      {/* Link Items */}
+      <div className="max-w-4xl mx-auto space-y-3">
+        <LinkItem shortened='https://rel.ink/hjtwxk' link='https://github.com/Admiral-Simo' />
+        <LinkItem shortened='https://rel.ink/mjqukd' link='https://payrolline.com' second />
+        <LinkItem  shortened='https://rel.ink/gjsqxk' link='https://google.com' />
+      </div>
+    </section>
+  );
+};
+
+export default Main;
